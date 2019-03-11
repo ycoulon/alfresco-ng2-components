a27b498f77efe2d849168cf939194894a8a792f2
maurizio vitale <maurizio.vitale@alfresco.com>
2019-03-11 10:45:43 +0000

    Add stuff to check form

--- demo-shell/src/app/components/app-layout/cloud/task-details-cloud-demo.component.ts
+++ demo-shell/src/app/components/app-layout/cloud/task-details-cloud-demo.component.ts
@@ -17,6 +17,7 @@
 
 import { Component } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { FormModel } from '@alfresco/adf-core';
 
 @Component({
     templateUrl: './task-details-cloud-demo.component.html',
@@ -28,6 +29,8 @@ export class TaskDetailsCloudDemoComponent {
     appName: string;
     readOnly = false;
 
+    formDefinition;
+
     constructor(private route: ActivatedRoute, private router: Router) {
         this.route.params.subscribe((params) => {
             this.taskId = params.taskId;
@@ -35,6 +38,68 @@ export class TaskDetailsCloudDemoComponent {
         this.route.parent.params.subscribe((params) => {
             this.appName = params.appName;
         });
+
+        this.formDefinition = new FormModel({
+            "id": "text-form",
+            "name": "test-start-form",
+            "description": "",
+            "version": 0,
+            "tabs": [],
+            "fields": [
+                {
+                    "type": "container",
+                    "id": "1511517333638",
+                    "name": "Label",
+                    "tab": null,
+                    "fieldType": "ContainerRepresentation",
+                    "fields": {
+                        "1": [
+                            {
+                                "type": "text",
+                                "id": "texttest",
+                                "name": "texttest",
+                                "fieldType": "FormFieldRepresentation",
+                                "colspan": 1,
+                                "params": {
+                                    "existingColspan": 2,
+                                    "maxColspan": 6,
+                                    "inputMaskReversed": true,
+                                    "inputMask": "0#",
+                                    "inputMaskPlaceholder": "(0-9)"
+                                },
+                                "visibilityCondition": null,
+                                "placeholder": "text",
+                                "value": null,
+                                "required": false,
+                                "minLength": 0,
+                                "maxLength": 0,
+                                "regexPattern": null
+                            }
+                        ],
+                        "2": []
+                    },
+                    "numberOfColumns": 2
+                }
+            ],
+            "outcomes": [],
+            "metadata": {
+                "property1": "value1",
+                "property2": "value2"
+            },
+            "variables": [
+                {
+                    "name": "variable1",
+                    "type": "string",
+                    "value": "value1"
+                },
+                {
+                    "name": "variable2",
+                    "type": "string",
+                    "value": "value2"
+                }
+            ]
+        });
+        this.formDefinition.taskId = this.taskId;
     }
 
     onGoBack() {
