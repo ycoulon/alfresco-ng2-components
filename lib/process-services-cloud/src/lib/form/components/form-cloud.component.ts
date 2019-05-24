141eb16f595d992b6d19346bc7f370bc6551c4df
sivakumar414ram <siva.kumar@muraai.com>
2019-05-24 17:56:51 +0530

    * Process-storage

--- lib/process-services-cloud/src/lib/form/components/form-cloud.component.ts
+++ lib/process-services-cloud/src/lib/form/components/form-cloud.component.ts
@@ -34,7 +34,7 @@ import { FormBaseComponent,
         FormFieldValidator } from '@alfresco/adf-core';
 import { FormCloudService } from '../services/form-cloud.service';
 import { FormCloud } from '../models/form-cloud.model';
-import { TaskVariableCloud } from '../models/task-variable-cloud.model';
+import { TaskVariableCloud, ProcessStorageCloudModel } from '../models/task-variable-cloud.model';
 import { DropdownCloudWidgetComponent } from './dropdown-cloud/dropdown-cloud.widget';
 import { UploadCloudWidgetComponent } from './upload-cloud.widget';
 
@@ -88,6 +88,7 @@ export class FormCloudComponent extends FormBaseComponent implements OnChanges, 
 
     protected subscriptions: Subscription[] = [];
     nodeId: string;
+    processStorageCloudModel: ProcessStorageCloudModel;
 
     protected onDestroy$ = new Subject<boolean>();
 
